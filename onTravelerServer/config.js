64b5abd387b8e091de63457185b6b3c2aca3bd17
hanhyeonmin <um4825@gmail.com>
2018-11-19 01:51:00 +0900

    make gitignore

--- onTravelerServer/config.js
+++ onTravelerServer/config.js
@@ -1,6 +1,6 @@
 var myip = require('quick-local-ip');
 
 module.exports = {
-    'mongo_url' : "mongodb://hhm:1234@localhost:27017/traveler",
-    'node_ip' : 'ec2-18-222-114-158.us-east-2.compute.amazonaws.com'
+    'mongo_url' : "mongodb://localhost:27017/traveler",
+    'node_ip' : 'localhost'
 };
