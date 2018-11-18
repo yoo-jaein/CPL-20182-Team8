a9cab2b0dcc321ac75db4dd934b1c6ae6426d6db
hanhyeonmin <um4825@gmail.com>
2018-11-19 01:29:36 +0900

    bug fix

--- onTravelerServer/routes/buddies.js
+++ onTravelerServer/routes/buddies.js
@@ -35,8 +35,8 @@ router.get('/location/:location_pos', function(req, res, next){
     pos = req.params.location_pos.split(/[ ,]+/);
 
     Buddy.find()
-        .where(active_location.langitude = pos[0])
-        .where(active_location.longitude = pos[1])
+        .where('active_location.langitude').equals(pos[0])
+        .where('active_location.longitude').equals(pos[1])
         .then(function(buddy){
             if(!buddy.length) return res.status(404).send(ReturnFormat.get_format(0, buddy, "cannot found"));
             res.send(ReturnFormat.get_format(1, buddy, ""));
