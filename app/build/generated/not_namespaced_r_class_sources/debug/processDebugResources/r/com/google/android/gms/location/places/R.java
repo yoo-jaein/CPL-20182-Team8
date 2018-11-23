216224679541637abace78602d05c37e7e94b03e
MinJeong <koomin1229@naver.com>
2018-11-24 02:46:35 +0900

    Change Maps search image

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/google/android/gms/location/places/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/google/android/gms/location/places/R.java
@@ -36,10 +36,10 @@ public final class R {
     public static final class drawable {
         private drawable() {}
 
-        public static final int places_ic_clear = 0x7f070193;
-        public static final int places_ic_search = 0x7f070194;
-        public static final int powered_by_google_dark = 0x7f070197;
-        public static final int powered_by_google_light = 0x7f070198;
+        public static final int places_ic_clear = 0x7f070194;
+        public static final int places_ic_search = 0x7f070195;
+        public static final int powered_by_google_dark = 0x7f070198;
+        public static final int powered_by_google_light = 0x7f070199;
     }
     public static final class id {
         private id() {}
