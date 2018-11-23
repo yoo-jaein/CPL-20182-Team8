7473fdcd02fafc826dfba383d3123959f75d6f83
MinJeong <koomin1229@naver.com>
2018-11-24 00:55:41 +0900

    Change app&button image

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/google/android/gms/location/places/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/google/android/gms/location/places/R.java
@@ -36,10 +36,10 @@ public final class R {
     public static final class drawable {
         private drawable() {}
 
-        public static final int places_ic_clear = 0x7f07018e;
-        public static final int places_ic_search = 0x7f07018f;
-        public static final int powered_by_google_dark = 0x7f070192;
-        public static final int powered_by_google_light = 0x7f070193;
+        public static final int places_ic_clear = 0x7f070192;
+        public static final int places_ic_search = 0x7f070193;
+        public static final int powered_by_google_dark = 0x7f070196;
+        public static final int powered_by_google_light = 0x7f070197;
     }
     public static final class id {
         private id() {}
